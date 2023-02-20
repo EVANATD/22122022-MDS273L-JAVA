12d9baf3adf9b3eec43ffa27cf0cbfa14b7fe7cb
EVANATD <118044613+EVANATD@users.noreply.github.com>
2023-02-20 14:55:43 +0530

    Update lab4.java

--- lab 4/lab4.java
+++ lab 4/lab4.java
@@ -1,32 +1,25 @@
+
+
 import java.util.Scanner;
 
-public class BankAccount {
-    
-    static int accountNumber;
-    static String accountHolderName;
-    static double accountBalance;
-    static String transactions = "";
-    
+public class BankAccount{
+    private static int accountNumber;
+    private static String accountHolderName;
+    private static double accountBalance;
+    private static Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter account number: ");
-        accountNumber = scanner.nextInt();
-        scanner.nextLine(); // to clear the buffer
-        System.out.print("Enter account holder name: ");
-        accountHolderName = scanner.nextLine();
-        System.out.print("Enter account balance: ");
-        accountBalance = scanner.nextDouble();
-        
-        int choice;
-        do {
-            System.out.println("\nMenu");
+        initializeCustomer();
+        int choice = 0;
+        boolean exit = false;
+        while (!exit) {
+            System.out.println("Menu:");
             System.out.println("1. Deposit money");
             System.out.println("2. Withdraw money");
             System.out.println("3. Print transactions");
             System.out.println("4. Print account summary");
             System.out.println("5. Exit");
-            System.out.print("Enter your choice: ");
-            choice = scanner.nextInt();
+            choice = Integer.parseInt(scanner.nextLine());
             switch (choice) {
                 case 1:
                     depositMoney();
@@ -41,55 +34,67 @@ public class BankAccount {
                     printAccountSummary();
                     break;
                 case 5:
-                    System.out.println("Exiting the program...");
+                    exit = true;
                     break;
                 default:
-                    System.out.println("Invalid choice. Please try again.");
+                    System.out.println("Invalid choice");
+                    break;
             }
-        } while (choice != 5);
-        
-        scanner.close();
-    }
-    
-    public static void initializeCustomer(int accNumber, String accHolderName, double accBalance) {
-        accountNumber = accNumber;
-        accountHolderName = accHolderName;
-        accountBalance = accBalance;
-    }
-    
-    public static void depositMoney() {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter amount to deposit: ");
-        double amount = scanner.nextDouble();
-        if (amount <= 0) {
-            System.out.println("Invalid amount. Please try again.");
-        } else {
-            accountBalance += amount;
-            transactions += "Deposited: " + amount + "\n";
-            System.out.println("Deposit successful.");
         }
     }
-    
+
+    public static void initializeCustomer() {
+        System.out.println("Enter account number:");
+        accountNumber = Integer.parseInt(scanner.nextLine());
+        System.out.println("Enter account holder name:");
+        accountHolderName = scanner.nextLine();
+        System.out.println("Enter account balance:");
+        accountBalance = Double.parseDouble(scanner.nextLine());
+        System.out.println("Account created with balance " + accountBalance);
+    }
+
+    public static void depositMoney() {
+        System.out.println("Enter amount to deposit:");
+        double amount = Double.parseDouble(scanner.nextLine());
+        double previousBalance = accountBalance;
+        accountBalance += amount;
+        String transactionDetails = "Deposited: " + amount + ", Previous Balance: " + previousBalance + ", New Balance: " + accountBalance + "\n";
+        System.out.println("Deposit successful");
+        addToTransactionHistory(transactionDetails);
+    }
+
     public static void withdrawMoney() {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter amount to withdraw: ");
-        double amount = scanner.nextDouble();
-        if (amount <= 0 || amount > accountBalance) {
-            System.out.println("Invalid amount. Please try again.");
+        System.out.println("Enter amount to withdraw:");
+        double amount = Double.parseDouble(scanner.nextLine());
+        double previousBalance = accountBalance;
+        if (amount > accountBalance) {
+            System.out.println("Insufficient balance");
         } else {
             accountBalance -= amount;
-            transactions += "Withdrawn: " + amount + "\n";
-            System.out.println("Withdrawal successful.");
+            String transactionDetails = "Withdrawn: " + amount + ", Previous Balance: " + previousBalance + ", New Balance: " + accountBalance + "\n";
+            System.out.println("Withdrawal successful");
+            addToTransactionHistory(transactionDetails);
         }
     }
-    
+
     public static void printTransactions() {
-        System.out.println("Transactions:\n" + transactions);
+        System.out.println("Transaction History:");
+        System.out.println(getTransactionHistory());
     }
-    
+
     public static void printAccountSummary() {
-        System.out.println("Account Number: " + accountNumber);
-        System.out.println("Account Holder Name: " + accountHolderName);
-        System.out.println("Account Balance: " + accountBalance);
+        System.out.println("Account number: " + accountNumber);
+        System.out.println("Account holder name: " + accountHolderName);
+        System.out.println("Account balance: " + accountBalance);
+    }
+
+    private static String transactionHistory = "";
+
+    private static void addToTransactionHistory(String transactionDetails) {
+        transactionHistory += transactionDetails;
+    }
+
+    private static String getTransactionHistory() {
+        return transactionHistory;
     }
 }
