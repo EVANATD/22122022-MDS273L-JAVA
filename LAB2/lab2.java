dc2953e60504696705fc9196d673d7f60ba51665
EVANATD <118044613+EVANATD@users.noreply.github.com>
2023-05-13 19:04:49 +0530

    m

--- LAB2/lab2.java
+++ LAB2/lab2.java
@@ -1,97 +1,92 @@
-import java.util.Scanner;
-public class lab2 {
-  static Scanner sc = new Scanner(System.in);
-  static String[] names = new String[1024];
-  static int count = 0;
+import java.util.*;
+public class lab2{
+    public static void main(String[] args){
+        String name;
+        int age;
+        String state;
+        char gender;
+        int salary;
+        String company;
+        Scanner scan = new Scanner(System.in);
+        System.out.print("enter name : ");
+        name=scan.nextLine();
+        System.out.print("enter age : ");
+        age=Integer.parseInt(scan.nextLine());
+        System.out.print("enter state : ");
+        state=scan.nextLine();
+        System.out.print("enter gender : ");
+        gender=scan.nextLine().charAt(0);
+        System.out.print("enter salary : ");
+        salary=Integer.parseInt(scan.nextLine());
+        System.out.print("Enter company name : ");
+        company=scan.nextLine();
+            
+        if(gender=='f'){
+            System.out.print("The employee " + name + " is female\n");
+        }else if(gender=='m'){
+                System.out.print("Employee " + name + " is male\n");
+            }
+            switch(state){
+                case "jammu and ashmir":
+                case "delhi":
+                case "uttar pradesh":
+                {
+                    System.out.print(name + "is coming from " + state + " which is the northern part of india\n");
+                    break;
+                }
 
-  public static void main(String[] args) {
-    int option;
-    do {
-      printMenu();
-        option = sc.nextInt();
-      switch (option) {
-        case 1:
-          addName();
-          break;
-        case 2:
-          searchName();
-          break;
-        case 3:
-          removeName();
-          break;
-        case 4:
-          System.out.println("Exiting from the program");
-          break;
-        default:
-          System.out.println("Invalid option, Please try again");
-          break;
-      }
-    } while (option != 4);
-  }
+            
+                case "gujarat":
+                case "goa":
+                case "madhyapradesh":
+                case "maharashtra":
+                {
+                    System.out.print(name + "is coming from " + state +" which is the western part of india\n");
+                    break;
+                }
 
-  static void printMenu() {
-    System.out.println("Menu:");
-    System.out.println("1. Enter a name");
-    System.out.println("2. Search for a name");
-    System.out.println("3. Remove a name");
-    System.out.println("4. Exit");
-    System.out.print("Enter your option: ");
-  }
+                case "westbengal":
+                case "bihar":
+                case "jharkhand":
+                case "odisha":
+                {
+                    System.out.print(name + "is coming from " + state + " which is the eastern part of india\n");
+                    break;
+                }
 
-  static void addName() {
-    System.out.print("Enter name: ");
-    String name = sc.next();
-    if (isNameExists(name)) {
-      System.out.println("This name already exist");
-    } else {
-      names[count++] = name;
-      System.out.println("Name added successfully");
-    }
-  }
+                case "kerala":
+                case "tamil nadu":
+                case "andhra pradesh":
+                case "karnataka":
+                case "telangana":
+                {
+                    System.out.print(name + "  is coming from " + state +" which is the southern part of india\n");
+                    break;
+                }
+                default:{
+                    System.out.print("invalid state");
+                    break;
+                }
+            }
+            switch(company){
+                case "Google":
+                case "Facebook":
+                case "Samsung":
+                case "IBM":
+                case "Apple":
+                System.out.print(name + " belong to the top MNC which is " + company);
+                break;
+            
+            default:
+                System.out.print(name + " does not belong to the top MNC");
+                break;
+            
 
-  static void searchName() {
-    System.out.print("Enter name to be searched: ");
-    String name = sc.next();
-    boolean isFound = false;
-    for (int i = 0; i < count; i++) {
-      if (names[i].equals(name)) {
-        System.out.println("Name found at position: " + (i + 1));
-        isFound = true;
-        break;
-      }
-    }
-    if (!isFound) {
-      System.out.println("Name not found.");
-    }
-  }
-
-  static void removeName() {
-    System.out.print("Enter name to be removed: ");
-    String name = sc.next();
-    boolean isFound = false;
-    for (int i = 0; i < count; i++) {
-      if (names[i].equals(name)) {
-        for (int j = i; j < count - 1; j++) {
-          names[j] = names[j + 1];
-        }
-        count--;
-        System.out.println("Name removed successfully");
-        isFound = true;
-        break;
-      }
-    }
-    if (!isFound) {
-      System.out.println("Name not found.");
-    }
-  }
-
-  static boolean isNameExists(String name) {
-    for (int i = 0; i < count; i++) {
-      if (names[i].equals(name)) {
-        return true;
-      }
-    }
-    return false;
-  }
-    
+            }
+          
+          
+          }
+          
+          
 }
+    
